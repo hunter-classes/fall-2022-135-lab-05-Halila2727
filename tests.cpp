2730ddf511b1e15fffb463dccb0769d32f00f51b
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-10-07 00:17:55 -0400

    created tests for isPrime(), nextPrime(), countPrimes() functions

--- tests.cpp
+++ tests.cpp
@@ -6,8 +6,30 @@
 TEST_CASE("Divisible By Cases")
 {
 	CHECK(isDivisibleBy(4, 2) == true);
-    CHECK(isDivisibleBy(-4, 2) == true);\
+    CHECK(isDivisibleBy(-4, 2) == true);
     CHECK(isDivisibleBy(14, 4) == false);
     CHECK(isDivisibleBy(2000, 0) == false);
 }
 
+TEST_CASE("Is Prime Cases")
+{
+    CHECK(isPrime(0) == false);
+    CHECK(isPrime(1) == false);
+    CHECK(isPrime(-3) == false);
+    CHECK(isPrime(69) == false);
+    CHECK(isPrime(89) == true);
+}
+
+TEST_CASE("Next Prime Cases")
+{
+    CHECK(nextPrime(0) == 2);
+    CHECK(nextPrime(-15) == 2);
+    CHECK(nextPrime(182) == 191);
+}
+
+TEST_CASE("Count Primes Cases")
+{
+    CHECK(countPrimes(-111111, 2) == 1);
+    CHECK(countPrimes(1, 100) == 25);
+    CHECK(countPrimes(150, 188) == 7);
+}
