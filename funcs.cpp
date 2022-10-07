957fa2f5b22976e6cd91b4e314049bedd2906c4a
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-10-07 00:16:49 -0400

    added isPrime(), nextPrime() and countPrimes() functions

--- funcs.cpp
+++ funcs.cpp
@@ -18,3 +18,56 @@ bool isDivisibleBy(int n, int d)
         return false;
     }
 }
+
+bool isPrime(int n)
+{
+    if(n < 2)
+    {
+        return false;
+    }
+    for(int i = 2; i < n; i++)
+    {
+        if(n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int nextPrime(int n)
+{
+    for(int i = n+1; i < 2147483647; i++)
+    {
+        if(isPrime(i) == true)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int countPrimes(int a, int b)
+{
+    int larger, smaller;
+    int count = 0;
+    if(a > b)
+    {
+        larger = a;
+        smaller = b;
+    }
+    else
+    {
+        larger = b;
+        smaller = a;
+    }
+
+    for(int i = smaller; i <= larger; i++)
+    {
+        if(isPrime(i) == true)
+        {
+            count++;
+        }
+    }
+    return count;
+}
